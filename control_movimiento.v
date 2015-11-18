c8052d7e5d3e202b12573a32ac89ddd0d41bc289
Gustavo Muñoz López <gjmunozl@unal.edu.co>
2015-11-18 03:56:37 -0500

    Update control_movimiento.v

--- control_movimiento.v
+++ control_movimiento.v
@@ -1,89 +1,80 @@
-module control_movimiento (s,clk, R_vertical_1 , R_vertical_2 , R_horizontal_1 , R_horizontal_2 , theta_manual , theta_actual , phi_manual , phi_actual, s_out_theta, s_out_phi);
-  
-  
-	input [1:0]s; //////////interruptor modo manual/automatico
-	input clk;
-	////////////////////entradas de la fotoresistencias.
-	input [15:0]R_vertical_1;
-	input [15:0]R_vertical_2;
-	input [15:0]R_horizontal_1;
-	input [15:0]R_horizontal_2;
-	///////////////////entradas de posición manual
-	input [15:0]theta_manual;
-	input [15:0]phi_manual;
-	///////////////////entradas de posición actual	
-	input [15:0]phi_actual;
-	input [15:0]theta_actual;
-	
-	//////////////////Salida para motores en eje teta y fi
-	output reg [1:0] s_out_theta;
-	output reg [1:0] s_out_phi;
+module control_movimiento (s,clk, R_vertical_1 , R_vertical_2 , R_horizontal_1 , R_horizontal_2 , /*theta_manual , theta_actual , phi_manual , phi_actual,*/ s_out_theta, s_out_phi);
 
-	//inicialización 
-	reg[1:0] shift_motor=2'b00;
-	reg[1:0] shift_R=2'b00;
-	reg [1:0] mover_teta;
-	reg [1:0] mover_fi;
-	reg [15:0]error=3'b101;
-	
-	
-	
-	always @(posedge clk)begin
 
-		////////////////MODO AUTOMATICO///////////////////////
-		if(shift_motor==2'b00)begin
+input [1:0]s;//--------------interruptor modo manual/automatico
+input clk;
 
-		////////////////comparador vertical
-			if ((R_vertical_1>=(R_vertical_2-error) )&& (R_vertical_1<=(R_vertical_2+error)))begin
-				mover_teta=2'b00; ////El valor de las fotoresis. estan equilibradas
-				shift_motor=2'b10;
-				
-			end else begin 
-				if(R_vertical_1>R_vertical_2)begin
-					mover_teta=2'b01;  // movimiento horario  vertical				
-				end
-				if(R_vertical_1<R_vertical_2)begin
-					mover_teta=2'b11;   // movimiento anti-horario  vertical
-				end
-			end 
-			s_out_theta=mover_teta;
-		///////////////////////////////////////
+input [15:0]R_vertical_1;//----------------entradas de la fotoresistencias.
+input [15:0]R_vertical_2;
+input [15:0]R_horizontal_1;
+input [15:0]R_horizontal_2;//--------------entradas de la fotoresistencias.
 
-		end else begin
-			///////////////comparador horizontal
-			if (R_horizontal_1>=(R_horizontal_2-error) && R_horizontal_1<=(R_horizontal_2+error))begin
-					mover_fi=2'b00; ////El valor de las fotoresis. estan equilibradas, el movimiento es cero.
-					shift_motor=2'b00;
-			end else begin 
-				if(R_horizontal_1>R_horizontal_2)begin
-					mover_fi=2'b01;  // movimiento horario  horizontal
-				end
-				if(R_horizontal_1<R_horizontal_2)begin
-					mover_fi=2'b11;   // movimiento anti-horario  horizontal
-				end
-			end
-			s_out_phi=mover_fi;
-		  /////////////////////////////////////
-		end
+/*
+input [15:0]theta_manual;//--------------entradas de posición manual
+input [15:0]phi_manual;//--------------entradas de posición actual	
+
+input [15:0]phi_actual;//--------------entradas de posición manual
+input [15:0]theta_actual;//--------------entradas de posición actual	
+*/
+
+output reg [1:0] s_out_theta;//--------------Salida para motores en eje teta y fi
+output reg [1:0] s_out_phi;//--------------Salida para motores en eje teta y fi
+
+//inicialización 
+reg[1:0] shift_motor=2'b00;
+reg[1:0] shift_R=2'b00;
+reg [1:0] mover_teta;
+reg [1:0] mover_fi;
+reg [15:0]error=3'b101;
+
+
+
+always @(posedge clk)begin
+
 	////////////////MODO AUTOMATICO///////////////////////
-		
-	
-	end
-	
-		
-endmodule
+	if(shift_motor==2'b00)begin
 
-always @(posedge clk) //comparación variables verticales AUTOMÁTICO
-  begin
-	if(init1)begin
-	    if (C>=(D+error)||C<=(D-error) 
-		begin    s_out_phi=0;end
-	    else if(C<D)
-		begin    s_out_theta=2'b01;end   //movimiesto motor hacia a izquierda 
-	   else if(C>D )
-		begin    s_out_theta=2'b10;end   //movimiesto motor hacia a derecha
+	////////////////comparador vertical
+		if ((R_vertical_1>=(R_vertical_2-error) )&& (R_vertical_1<=(R_vertical_2+error)))begin
+			mover_teta=2'b00; ////El valor de las fotoresis. estan equilibradas
+			shift_motor=2'b10;
+			
+		end else begin 
+			if(R_vertical_1>R_vertical_2)begin
+				mover_teta=2'b01;  // movimiento horario  vertical				
+			end
+			if(R_vertical_1<R_vertical_2)begin
+				mover_teta=2'b11;   // movimiento anti-horario  vertical
+			end
+		end 
+		s_out_theta=mover_teta;
+	///////////////////////////////////////
+
+	end else begin
+		///////////////comparador horizontal
+		if (R_horizontal_1>=(R_horizontal_2-error) && R_horizontal_1<=(R_horizontal_2+error))begin
+				mover_fi=2'b00; ////El valor de las fotoresis. estan equilibradas, el movimiento es cero.
+				shift_motor=2'b00;
+		end else begin 
+			if(R_horizontal_1>R_horizontal_2)begin
+				mover_fi=2'b01;  // movimiento horario  horizontal
+			end
+			if(R_horizontal_1<R_horizontal_2)begin
+				mover_fi=2'b11;   // movimiento anti-horario  horizontal
+			end
+		end
+		s_out_phi=mover_fi;
+	  /////////////////////////////////////
 	end
-  end
+////////////////MODO AUTOMATICO///////////////////////
+	
+	
+	end
+	
+		
+
+
+/*
 
 
 //MODOD MANUAL
@@ -138,7 +129,7 @@ always @(posedge clk)  //movimento en el eje fi o horizontal
 
 
 
-
+*/
 
 
 endmodule
