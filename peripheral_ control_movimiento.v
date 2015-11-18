3b9e6e0ae42f1cddeb34260f8e563b35f3adc40a
Gustavo Muñoz López <gjmunozl@unal.edu.co>
2015-11-18 17:44:51 -0500

    Update peripheral_ control_movimiento.v

--- peripheral_ control_movimiento.v
+++ peripheral_ control_movimiento.v
@@ -11,7 +11,6 @@ module peripheral_control_movimiento(clk , rst , d_in , cs , addr , rd , wr, d_o
 
 
 //------------------------------------ regs and wires-------------------------------
-
 reg [1:0] SS=0;
 reg [15:0] RV1=0;
 reg [15:0] RV2=0;
@@ -19,6 +18,8 @@ reg [15:0] RH1=0;
 reg [15:0] RH2=0;
 wire[1:0] s_out_theta;
 wire[1:0] s_out_phi;
+
+reg [5:0] s;
 //------------------------------------ regs and wires-------------------------------
 
 
@@ -33,15 +34,17 @@ case (addr)
 4'h6:begin s = (cs && wr) ? 6'b000100 : 6'b000000 ;end //RH1
 4'h8:begin s = (cs && wr) ? 6'b000101 : 6'b000000 ;end //RH2
 
-
+4'hA:begin s = (cs && rd) ? 6'b001000 : 6'b000000 ;end //s_out_theta
+4'hC:begin s = (cs && rd) ? 6'b010000 : 6'b000000 ;end //s_out_phi
 default:begin s = 6'b000000 ; end
 endcase
-end
+end//-----------------address_decoder--------------------------
 
 
 
 
-//-----------------address_decoder--------------------------
+
+
 
 
 always @(negedge clk) begin//-------------------- escritura de registros 
@@ -52,17 +55,28 @@ case(s)
 6'b000011 : RV2 = d_in;
 6'b000100 : RH1 = d_in;
 6'b000101 : RH2 = d_in;
-
-
-default:begin s = 6'b000000 ; endcase
+default:begin s = 6'b000000 ;end
+endcase
 end//------------------------------------------- escritura de registros 
 
 
+always @(negedge clk) begin//-----------------------mux_4 :   multiplexa salidas del periferico
+case (s[5:3])
+3'b001: d_out = s_out_theta;
+3'b010: d_out = s_out_phi;
+default: d_out   = 0 ;
+endcase
+end//-------------------------------------mux_4
 
 
-
-
-
-
+control_movimiento control_movimiento(
+					.s(SS),
+					.clk(clk),
+					.R_vertical_1(RV1),
+					.R_vertical_2(RV2),
+					.R_horizontal_1(RH1),
+					.R_horizontal_2(RH2),
+					.s_out_theta(s_out_theta),
+					.s_out_phi(s_out_phi));
 
 endmodule
