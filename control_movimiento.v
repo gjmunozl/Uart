88b856c15a36668ce345997f6d124c6e1666ecfc
calgaviriabo <calgaviriabo@unal.edu.co>
2015-11-26 19:23:13 -0500

    Update control_movimiento.v

--- control_movimiento.v
+++ control_movimiento.v
@@ -1,3 +1,5 @@
+`timescale 1ns / 1ps
+
 module control_movimiento (s,clk, R_vertical_1 , R_vertical_2 , R_horizontal_1 , R_horizontal_2 , theta_manual , theta_actual , phi_manual , phi_actual, s_out_theta_pos, s_out_theta_neg, s_out_phi_pos, s_out_phi_neg);
   
 	input clk;
@@ -31,78 +33,116 @@ module control_movimiento (s,clk, R_vertical_1 , R_vertical_2 , R_horizontal_1 ,
 	always @(posedge clk)begin
 	
 	
-		if(s)begin
-			//----------------------//MODO MANUAL//----------------------//
+		if(s!=1)begin
+		//----------------------//MODO AUTOMATICO//----------------------//	
 			if(shift_motor==2'b00)begin
+			
+				////////////////mover automaticamente motor theta
+				
+				//NO mover motor phi
+				s_out_phi_pos=2'b00;
+				s_out_phi_neg=2'b00;
+	
+				if ((R_vertical_1>=(R_vertical_2-error) )&& (R_vertical_1<=(R_vertical_2+error)))begin
+					//NO mover motor theta
+					s_out_theta_neg=2'b00;
+					s_out_theta_pos=2'b00;
+					shift_motor=2'b10;
+				end else begin 
+					if(R_vertical_1>R_vertical_2)begin
+						s_out_theta_pos=2'b01; // movimiento horario vertical
+					end
+					if(R_vertical_1<R_vertical_2)begin
+						s_out_theta_neg=2'b01; // movimiento anti-horario vertical
+					end	
+				end
+				////////////////mover automaticamente motor theta
+				
+			end else begin		
+			
+				////////////////mover automaticamente motor phi
+
+				//NO mover motor theta
+				s_out_theta_pos=2'b00;
+				s_out_theta_neg=2'b00;
+				
+				if (R_horizontal_1>=(R_horizontal_2-error) && R_horizontal_1<=(R_horizontal_2+error))begin
+						//NO mover motor phi
+						s_out_phi_pos=2'b00;
+						s_out_phi_neg=2'b00;
+						shift_motor=2'b00; 
+				end else begin 
+					if(R_horizontal_1>R_horizontal_2)begin
+						s_out_phi_pos=2'b01; // movimiento horario  horizontal
+					end	
+					if(R_horizontal_1<R_horizontal_2)begin
+						s_out_phi_neg=2'b01; // movimiento anti-horario horizontal
+					end	
+				end
+				////////////////mover automaticamente motor phi
+			end
+		//----------------------//MODO AUTOMATICO//----------------------//	
+		end else begin
+		//----------------------//MODO MANUAL//----------------------//
+			
+			if(shift_motor==2'b00)begin
+				
 				////////////////mover manualmente motor phi
-				if(phi_actual>=(phi_manual+error)||phi_actual<=(phi_manual-error))begin 
+				
+				//NO mover motor theta
+				s_out_theta_pos=2'b00;
+				s_out_theta_neg=2'b00;
+					
+				if(phi_actual>=(phi_manual+error)||phi_actual<=(phi_manual-error))begin 	
 					if(phi_actual>phi_manual) begin
 						if((phi_actual-phi_manual)<=giro) begin
-							s_out_phi_pos=2'b01;end	// movimiento horario horizontal
-						else begin
-							s_out_phi_neg=2'b01;end	// movimiento anti-horario horizontal
+							s_out_phi_pos=2'b01;	// movimiento horario horizontal
+							s_out_phi_neg=2'b00;
+						end else begin
+							s_out_phi_pos=2'b00;
+							s_out_phi_neg=2'b01; // movimiento anti-horario horizontal
+						end	
 					end else begin
 						if((phi_manual-phi_actual)<=giro) begin
-							s_out_phi_neg=2'b01;end	// movimiento anti-horario horizontal
-						else begin
-							s_out_phi_pos=2'b01;end	// movimiento horario horizontal
+							s_out_phi_pos=2'b00;
+							s_out_phi_neg=2'b01; // movimiento anti-horario horizontal
+						end else begin
+							s_out_phi_pos=2'b01; // movimiento horario horizontal
+							s_out_phi_neg=2'b00;
+						end
 					end
 				end else begin 
-					s_out_phi_pos=2'b00;///NO mover
-					s_out_phi_neg=2'b00;///NO mover
-					shift_motor=2'b01;	
+					shift_motor=2'b01;
+					//NO mover motor phi
+					s_out_phi_pos=2'b00;
+					s_out_phi_neg=2'b00;
 				end
 				////////////////mover manualmente motor phi
 			end else begin
-				////////////////mover manualmente motor theta
-				if(theta_actual>=(theta_manual+error)||theta_actual<=(theta_manual-error))begin 
-					if(theta_actual>theta_manual) begin
-						s_out_theta_pos=2'b01; end	// movimiento horario vertical
-					else begin 
-						s_out_theta_neg=2'b01; end	// movimiento anti-horario vertical
-				end else begin 
-					s_out_theta_neg=2'b00;///NO mover
-					s_out_theta_pos=2'b00;///NO mover
-					shift_motor=2'b01;
-				end
 				
 				////////////////mover manualmente motor theta
-			end
-			//----------------------//MODO MANUAL//----------------------//
-		end else begin
-
-			//----------------------//MODO AUTOMATICO//----------------------//
-			if(shift_motor==2'b00)begin
-
-				////////////////comparador vertical
-				if ((R_vertical_1>=(R_vertical_2-error) )&& (R_vertical_1<=(R_vertical_2+error)))begin
-					//las resistencias son casi iguales
-					s_out_theta_neg=2'b00;
+				
+				//NO mover motor phi
+				s_out_phi_pos=2'b00;
+				s_out_phi_neg=2'b00;
+				
+				if(theta_actual>=(theta_manual+error)||theta_actual<=(theta_manual-error))begin 
+					if(theta_actual>theta_manual) begin
+						s_out_theta_pos=2'b01; // movimiento horario vertical
+						s_out_theta_neg=2'b00; 
+					end else begin 
+						s_out_theta_pos=2'b00;
+						s_out_theta_neg=2'b01; // movimiento antihorario vertical
+					end	
+				end else begin 
+					shift_motor=2'b01;
+					//NO mover motor theta
 					s_out_theta_pos=2'b00;
-					shift_motor=2'b10;end 			
-				else begin 
-					if(R_vertical_1>R_vertical_2)begin
-						s_out_theta_pos=2'b01; end	// movimiento horario vertical
-					if(R_vertical_1<R_vertical_2)begin
-						s_out_theta_neg=2'b01; end	// movimiento anti-horario vertical
+					s_out_theta_neg=2'b00;
 				end
-				////////////////comparador vertical
-			end else begin		
-				////////////////comparador horizontal
-				if (R_horizontal_1>=(R_horizontal_2-error) && R_horizontal_1<=(R_horizontal_2+error))begin
-						//las resistencias son casi iguales
-						s_out_phi_pos=2'b00;
-						s_out_phi_neg=2'b00;
-						shift_motor=2'b00; end 
-				else begin 
-					if(R_horizontal_1>R_horizontal_2)begin
-						s_out_phi_pos=2'b01; end	// movimiento horario  horizontal
-					if(R_horizontal_1<R_horizontal_2)begin
-						s_out_phi_neg=2'b01; end	// movimiento anti-horario horizontal
-				end
-				////////////////comparador horizontal
+				////////////////mover manualmente motor theta
 			end
-			//----------------------//MODO MANUAL//----------------------//
+		//----------------------//MODO MANUAL//----------------------//
 		end	
 	end
 endmodule
